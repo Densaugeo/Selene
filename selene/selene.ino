76f630adcad99e39f6519bbaf4342cf3cfb3e8f4
Densaugeo <you@example.com>
2016-06-06 18:02:42 -0400

    Demo prep

--- selene/selene.ino
+++ selene/selene.ino
@@ -1,13 +1,32 @@
 #include "Skirnir.hpp"
 #include "Selene_Device.hpp"
+#include "Selene_AIn.hpp"
+#include "Selene_DIn.hpp"
 #include "Selene_DOut.hpp"
+#include "Selene_PWM.hpp"
 
-#define PB_DEBOUNCE 100
+#include <EEPROM.h>
+
+/*class SaveReporter : public Selene::DOut {
+  public:
+    SaveReporter(uint8_t selenePin, uint8_t arduinoPin, uint8_t* info, uint8_t infoSize):
+    DOut(selenePin, arduinoPin, info, infoSize) {
+      setState(EEPROM.read(selenePin));
+    }
+    
+    void saveState(uint32_t seleneAddress) {
+      EEPROM.update(selenePin, getState());
+    }
+};*/
+
+
+/*#define PB_DEBOUNCE 100
 
 int pb_previous = 0;
-int pb_time = 0;
-unsigned char packet_decoded[45];
-unsigned long last_ping = 0;
+int pb_time = 0;*/
+uint8_t packet_decoded[45];
+uint16_t last_ping = 0;
+uint16_t last_analog = 0;
 
 Skirnir a_skirnir = Skirnir(&Serial);
 
@@ -16,51 +35,70 @@ void send_with_skirnir(uint8_t payload[], uint8_t size) {
 }
 
 Selene::Pin** pins = new Selene::Pin*[3];
-Selene::Device a_device = Selene::Device(1, pins, 3, (uint8_t*) "{\"name\":\"Selene One\",\"desc\":\"Very first Selene device\",\"min\":0,\"max\":1}", 71, &send_with_skirnir);
+Selene::Device a_device = Selene::Device(1, pins, 3, (uint8_t*) "{\"name\":\"Selene One\",\"desc\":\"Very first Selene device\",\"min\":0,\"max\":255}", 73, &send_with_skirnir);
+
+Selene::Pin** pins_two = new Selene::Pin*[2];
+Selene::Device device_two = Selene::Device(2, pins_two, 2, (uint8_t*) "{\"name\":\"PB + LED\",\"min\":0,\"max\":1}", 35, &send_with_skirnir);
+
+Selene::Pin** pins_three = new Selene::Pin*[1];
+Selene::Device device_three = Selene::Device(3, pins_three, 1, (uint8_t*) "{\"name\":\"Analog Pin\",\"desc\":\"Read Voltage (out of 5V)\"}", 55, &send_with_skirnir);
+
 
 void setup() {
-  a_device.pins[0] = new Selene::DOut(0, 2, (uint8_t*) "{\"name\":\"Red\"}", 14);
-  a_device.pins[1] = new Selene::DOut(1, 3, (uint8_t*) "{\"name\":\"Green\"}", 16);
-  a_device.pins[2] = new Selene::DOut(2, 4, (uint8_t*) "{\"name\":\"Blue\"}", 15);
+  a_device.pins[0] = new Selene::PWM(0, 11, (uint8_t*) "{\"name\":\"Red\"}", 14);
+  a_device.pins[1] = new Selene::PWM(1, 10, (uint8_t*) "{\"name\":\"Green\"}", 16);
+  a_device.pins[2] = new Selene::PWM(2, 9, (uint8_t*) "{\"name\":\"Blue\"}", 15);
 
-  pinMode(5, INPUT);
-  digitalWrite(5, HIGH); // Turn on pullup
-  
+  device_two.pins[0] = new Selene::DIn(7, 7, true, (uint8_t*) "{\"name\":\"Pushbutton\"}", 21);
+  device_two.pins[1] = new Selene::DOut(5, 5, (uint8_t*) "{\"name\":\"Red LED\"}", 18);
+
+  device_three.pins[0] = new Selene::AIn(0, 5, (uint8_t*) "{\"name\":\"V\",\"min\":0,\"max\":1023}", 31);
+
+  //pinMode(5, INPUT);
+  //digitalWrite(5, HIGH); // Turn on pullup
+
   Serial.begin(115200);
 }
 
 void loop() {
-  // Check timer for overflow
-  if(millis() < pb_time) {
-    pb_time = 0;
-  }
+  uint32_t time = millis();
   
   // Start heartbeat every 2s
-  if(millis() > last_ping + 2000) {
+  if((uint16_t) time - last_ping > 2000) {
+    last_ping = time;
+
     a_skirnir.heartbeat();
-    last_ping = millis();
-  } else if(millis() < last_ping) {
-    // Reset after timer overflow
-    last_ping = 0;
   }
 
   // Returns true if a valid packet is found
   if(a_skirnir.receive_until_packet(packet_decoded)) {
     a_device.handlePacket(packet_decoded);
+    device_two.handlePacket(packet_decoded);
+    device_three.handlePacket(packet_decoded);
   }
 
-  if(!digitalRead(5) == 0) {
+  /*if(!digitalRead(5) == 0) {
     pb_previous = 0;
   } else if(pb_previous == 0 && millis() > pb_time + PB_DEBOUNCE) {
     pb_previous = 1;
     pb_time = millis();
 
     a_device.pins[0] -> setState(!a_device.pins[0] -> getState());
-  }
+  }*/
 
   // Check if pins needs to send updates
-  a_device.sendPinUpdates(millis());
+  a_device.sendPinUpdates(time);
+  device_two.sendPinUpdates(time);
+
+  if((uint16_t) time - last_analog > 100) {
+    last_analog = time;
+
+    //device_three.sendAllPins = true;
+    device_three.sendPinUpdates(time);
+  }
+
   
-  a_device.savePinStates(millis());
+  
+  //a_device.savePinStates(millis());
 }
 
